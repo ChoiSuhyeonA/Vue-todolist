bf96c59d82c173ebf503bf90771bf6f17471c4de
irlink2800 <irlink2800@gmail.com>
2021-08-13 15:52:40 +0900

    first

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="clearAllContainer" @click="clearTodo" >
+    <span class="clearAllBtn" >Clear All</span>
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    clearTodo(){
+     this.$emit('removeAll')
+    }
+  }
+}
+</script>
+
+<style>
+  .clearAllContainer{
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: rgb(15, 228, 111);
+    border-radius: 10px;
+    margin: 0 auto;
+  }
+  .clearAllBtn{
+    color:white;
+    display:black;
+  }
+</style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>Todo it! (author: ChoiSuhyeon)</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  h1{
+    color: #32528d;
+      font-weight:900;
+     margin: 2.5rem 0 1.5rem;
+  }
+</style>
